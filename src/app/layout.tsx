6d01966224386984945421b4c9e45e178cdbc943
solomonbaez <solomon.james.baez@gmail.com>
2023-10-31 17:03:27 -0700

    Added basic gsap infinite carousel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { GeistSans, GeistMono } from 'geist/font'
+import { GeistMono } from 'geist/font'
 import './globals.css'
 
 export const metadata: Metadata = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,58 @@
+"use client";
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-export default function Home() {
+gsap.registerPlugin(ScrollTrigger);
+
+const Home: React.FC = () => {
+  useEffect(() => {
+    let sections = gsap.utils.toArray<HTMLElement>("div"),
+      currentSection = sections[0];
+
+    gsap.defaults({overwrite: 'auto', duration: 0.3});
+
+    // stretch out the body height according to however many sections there are. 
+    gsap.set("body", {height: (sections.length * 100) + "%"});
+
+    // create a ScrollTrigger for each section
+    sections.forEach((section, i) => {
+      ScrollTrigger.create({
+        // use dynamic scroll positions based on the window height (offset by half to make it feel natural)
+        start: () => (i - 0.5) * innerHeight,
+        end: () => (i + 0.5) * innerHeight,
+        // when a new section activates (from either direction), set the section accordinglyl.
+        onToggle: self => self.isActive && setSection(section)
+      });
+    });
+
+    function setSection(newSection: HTMLElement) {
+      if (newSection !== currentSection) {
+        gsap.to(currentSection, {scale: 0.8, autoAlpha: 0})
+        gsap.to(newSection, {scale: 1, autoAlpha: 1});
+        currentSection = newSection;
+      }
+    }
+
+    // handles the infinite part, wrapping around at either end....
+    ScrollTrigger.create({
+      start: 1,
+      end: () => ScrollTrigger.maxScroll(window) - 1,
+      onLeaveBack: self => self.scroll(ScrollTrigger.maxScroll(window) - 2),
+      onLeave: self => self.scroll(2)
+    }).scroll(2);
+  }, [])
+
   return (
     <main>
       <div className="h-screen w-screen flex items-center justify-center">
         <h1 className="hover:text-blue-300">Hi, I&apos;m Solomon!</h1>
       </div>
+      <div className="h-screen w-screen flex items-center justify-center">
+        <h1>This is my portfolio 🌘</h1>
+      </div>
     </main>
   )
 }
+
+export default Home;
