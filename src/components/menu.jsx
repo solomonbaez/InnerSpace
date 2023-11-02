731aba6b0d436db577a83949b190d8ad96f03735
solomonbaez <solomon.james.baez@gmail.com>
2023-11-01 18:07:00 -0700

    Adjusted pixellation for mobile performance and changed color variation

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -4,21 +4,22 @@ const animate = {
       opacity: 0
   },
   open: {
-      opacity: 1
+      opacity: 1,
   },
   exit: {
-      opacity: 0
+      opacity: 0,
+      transition: {duration: 0, delay: 0.6}
   }
 }
 
 export default function Index({isActive}) {
   return (
-    <motion.div variants={animate} initial="initial" animate={isActive ? "open" : "closed"}
-      className="h-[90vh] w-full z-20 fixed flex flex-col items-center justify-center"
+    <motion.div variants={animate} initial="initial" animate={isActive ? "open" : "exit"}
+      className="h-[90vh] w-full z-20 fixed flex flex-col items-center justify-center uppercase"
     >
-      <p className="text-4xl m-1 mb-2">Home</p>
-      <p className="text-4xl m-1 mb-2">About</p>
-      <p className="text-4xl m-1 mb-2">Contact</p>
+      <p className="text-6xl text-black  m-1 mb-2">Home</p>
+      <p className="text-6xl text-black m-1 mb-2">About</p>
+      <p className="text-6xl text-black m-1 mb-2">Contact</p>
     </motion.div>
   )
 }
