7eee4b8b23b121a43996ce0c5783139e98e03e50
solomonbaez <solomon.james.baez@gmail.com>
2023-11-01 23:44:48 -0700

    Fixed diamond menu animation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,14 +20,15 @@ export default function Index( {isActive, setIsActive}: {isActive: boolean, setI
     return (
       <>
         <button
-        className="fixed top-10 right-10 z-30 w-20 h-20 flex items-center justify-center transform transition-transform"
+        className="bg-white fixed top-10 right-10 z-30 w-20 h-20 flex items-center justify-center"
         onClick={() => setIsActive(!isActive)}
         >
-          <div className="w-7">
-              <div className={`h-0.5 block bg-white relative transition-transform duration-300 ease-in-out -top-8 -right-1 ${isActive ? "rotate-45" : "-rotate-45"}`}></div>
-              <div className={`h-0.5 block bg-white relative transition-transform duration-300 ease-in-out -top-3 -right-1 ${isActive ? "-rotate-45" : "rotate-45 "}`}></div>
-              <div className={`h-0.5 block bg-white relative transition-transform duration-300 ease-in-out -top-9 -right-7 ${isActive ? "-rotate-45" : "rotate-45 "}`}></div>
-              <div className={`h-0.5 block bg-white relative transition-transform duration-300 ease-in-out -top-4 -right-7 ${isActive ? "rotate-45" : "-rotate-45 "}`}></div>
+          <div className="flex flex-row">
+              <div className={`h-0.5 w-5 bg-black relative transition-transform duration-300 ease-in-out -top-2 -right-5 ${isActive ? "translate-x-1 rotate-45" : "-rotate-45"}`}></div>
+              <div className={`h-0.5 w-5 bg-black relative transition-transform duration-300 ease-in-out -top-2 -right-5 ${isActive ? "-translate-x-1 -rotate-45" : "rotate-45"}`}></div>
+              
+              <div className={`h-0.5 w-5 bg-black relative transition-transform duration-300 ease-in-out top-2 right-5 ${isActive ? "translate-x-1 -translate-y-1 -rotate-45" : "rotate-45"}`}></div>
+              <div className={`h-0.5 w-5 bg-black relative transition-transform duration-300 ease-in-out top-2 right-5 ${isActive ? "-translate-x-1 -translate-y-1 rotate-45" : "-rotate-45"}`}></div>
           </div>
         </button>
       </>
