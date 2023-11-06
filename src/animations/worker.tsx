9f3b394b9e607c2a185825f4f90bacdbe9ceffc2
solomonbaez <solomon.james.baez@gmail.com>
2023-11-06 13:15:01 -0800

    Cleaning up unused code

--- src/animations/worker.tsx
+++ src/animations/worker.tsx
@@ -1,16 +1,4 @@
 import { render } from '@react-three/offscreen'
 import Ripple from "./ripple";
 
-interface Dimensions {
-  width: number,
-  height: number,
-  dpr: number,
-}
-
-const ripple: Dimensions = {
-  width: 640,
-  height: 640,
-  dpr: 1,
-}
-
-render(<Ripple dimensions={ripple}/>)
+render(<Ripple />)
